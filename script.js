c34df996345b3124b88ff292147466cbc77fc70d
coco <cocoxcomputerscience@gmail.com>
2024-06-02 13:55:24 -0400

    Complete intended functionality and fix clearForm
    Fix:
    - clearForm function now clears read status
    
    Newly added books now have:
    - a delete button
    - a toggle switch for read status

--- script.js
+++ script.js
@@ -7,19 +7,86 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(e) {
+    let index = e.currentTarget.dataset.index;
+    myLibrary[index].toggleRead();
+}
+
 function addToLibrary() {
     let isRead = inputRead.checked;
     let book = new Book(inputTextList[0].value, inputTextList[1].value, inputTextList[2].value, isRead)
     myLibrary.push(book);
 }
 
+function deleteBook(e) {
+    let index = e.currentTarget.dataset.index;
+    myLibrary.splice(index, 1);
+    let book = document.querySelector(`div[data-index='${index}']`);
+    book.parentNode.removeChild(book);
+}
+
 function displayBook() {
+    let index = myLibrary.length - 1
+    let newBook = myLibrary[index];
+    let cardImgWrapper = document.createElement("i");
+    let cardImg = document.createElement("img");
     let card = document.createElement("div");
-    let newBook = myLibrary[myLibrary.length - 1];
+    let delBtn = document.createElement("button");
+    delBtn.textContent = "X"
+
+    delBtn.dataset.index = index;
+    delBtn.classList.add("del-btn");
+    delBtn.addEventListener("click", deleteBook);
+    cardImg.setAttribute("src", "./book-icon.svg");
+    cardImgWrapper.appendChild(cardImg);
+    card.classList.add("card");
+    // used to identify book
+    card.dataset.index = index;
+    card.appendChild(delBtn);
+    card.appendChild(cardImgWrapper);
+
     for (property in newBook) {
-        let line = document.createElement("div");
-        line.textContent = `${property}: ${newBook[property]}`;
-        card.appendChild(line);
+        let div = document.createElement("div");
+        let para = document.createElement("p");
+
+        switch(property) {
+            case "title":
+                para.textContent = `"${newBook[property]}"`;
+                div.appendChild(para);
+                break;
+            case "author":
+                para.textContent = `by ${newBook[property]}`;
+                div.appendChild(para);
+                break;
+            case "pages":
+                para.textContent = `pages: ${newBook[property]}`;
+                div.appendChild(para);
+                break;
+            case "read":
+                div.classList.add("read-container");
+                para.textContent = "Read: "
+                let label = document.createElement("label");
+                let input = document.createElement("input");
+                let span = document.createElement("span");
+
+                if (newBook[property] === true) input.checked = true;
+                div.classList.add("read-container")
+                label.classList.add("read-switch");
+                input.setAttribute("type", "checkbox");
+                input.addEventListener("click", toggleRead);
+                input.dataset.index = index;
+                span.classList.add("slider");
+                label.appendChild(input);
+                label.appendChild(span)
+                div.appendChild(para);
+                div.appendChild(label);
+                break;
+        }
+        card.appendChild(div);
     }
 
     library.appendChild(card);
@@ -36,6 +103,7 @@ function displayError() {
 function clearForm() {
     inputTextList.forEach(inputText => inputText.value = "");
     errorTextList.forEach(errorText => errorText.textContent = "");
+    inputRead.checked = false;
 }
 
 let addBookButton = document.querySelector("#add-book-button");
